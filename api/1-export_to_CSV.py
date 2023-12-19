6a65ceebcdc893249e6d4a702c3f41d810fc7f52
Ergi Lama <ergilana98@gmail.com>
2023-12-19 18:26:48 +0100

    fox

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,27 +4,24 @@ Model to make a request to an
 API and retrieve data
 """
 
+
 import json
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    """
-    Main function to fetch data from the API and write to the CSV file.
-    """
     URL = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
     res = requests.get(f"{URL}users/{argv[1]}")
     res = res.json()
-    user_name = res['name']
+    user_name = res['username']
 
     res = requests.get(f"{URL}todos")
     all_todos = res.json()
     user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
 
-    with open(f"{iser_id}.csv", 'w', newline='') as file:
-              writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-              writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-              for task in user_todos:
-                  writer.writerow([user_id, user_name, task["completed"], task["title"0]])
+    with open(f"{user_id}.csv", 'w') as csv:
+        for i, todo in enumerate(user_todos):
+            first = f'"{user_id}","{user_name}",'
+            csv.write(f'{first}"{todo["completed"]}","{todo["title"]}"\n')
